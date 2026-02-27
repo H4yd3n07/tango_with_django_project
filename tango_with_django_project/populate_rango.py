88f6323a042d3cc5fe7a3cad932a1f337c7b8427
Hayden Gilmour <3165997G@student.gla.ac.uk>
2026-02-27 17:52:55 +0000

    Exercises complete and Chapter 5 tests passed.

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -19,7 +19,9 @@ def populate():
     {'title':'How to Think like a Computer Scientist',
     'url':'http://www.greenteapress.com/thinkpython/'},
     {'title':'Learn Python in 10 Minutes',
-    'url':'http://www.korokithakis.net/tutorials/python/'} ]
+    'url':'http://www.korokithakis.net/tutorials/python/'},
+    
+      ]
 
     django_pages = [{'title':'Official Django Tutorial',
     'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
@@ -34,9 +36,10 @@ def populate():
     {'title':'Flask',
     'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages},
-    'Django': {'pages': django_pages},
-    'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages,'views':128,'likes':64},
+    'Django': {'pages': django_pages,'views':64,'likes':32},
+    'Other Frameworks': {'pages': other_pages,'views':32,'likes':16}, 
+    }
 
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
@@ -44,7 +47,7 @@ def populate():
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat,cat_data['views'],cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -60,8 +63,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p 
 
-def add_cat(name):
+def add_cat(name,views=0,likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.likes = likes 
+    c.views = views
     c.save()
     return c
 
