fbb934f225f6fab1b5795eb053dd146b3c27fd3d
Hayden Gilmour <3165997G@student.gla.ac.uk>
2026-03-01 16:18:22 +0000

    Ch7 main tasks complete, moving onto exercises.

---
+++ tango_with_django_project/rango/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from rango.models import Page, Category
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(
+        max_length=128,
+        help_text="Please enter the category name."
+    )
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        # Associate the ModelForm with the Category model
+        model = Category
+        fields = ('name',)  # Only expose `name` on the form
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=128,
+        help_text="Please enter the title of the page."
+    )
+    url = forms.URLField(
+        max_length=200,
+        help_text="Please enter the URL of the page."
+    )
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        # Associate the ModelForm with the Page model
+        model = Page
+        # Hide the foreign key; it will be set in the view when saving the form
+        exclude = ('category',)
+
+    # Optional: normalize URLs so tests/pages work even if scheme is omitted.
+    # If your coursework expects strict URL validation, you can remove this.
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
